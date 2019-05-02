de2b8c7df55c0a5ff7ef283b0aa18ed95ff649ee
강서연 <gsy0122@dgsw.hs.kr>
2019-05-02 10:09:41 +0900

    [ADD] gitignore

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import './Header.css';
 
 class Header extends Component {
 	render() {
 		return(
 			<header className="App-header">
         <ul>
-          <li><a href="">Q&A</a></li>
-          <li><a href="">질문하기</a></li>
-          <li><a href="">답변하기</a></li>
-          <li><a href="">프로필</a></li>
+          <li><a href="/">Q&A</a></li>
+          <li><a href="/question">질문하기</a></li>
+          <li><a href="/answer">답변하기</a></li>
+          <li><a href="/profile">프로필</a></li>
         </ul>
       </header>
 		);
