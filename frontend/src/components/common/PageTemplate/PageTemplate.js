3efdf68b1cde64709bf8d32ab2ed861f689ff82d
강서연 <gsy0122@dgsw.hs.kr>
2019-05-08 15:06:36 +0900

    [FIX] PageTemplate

--- frontend/src/components/common/PageTemplate/PageTemplate.js
+++ frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -2,11 +2,16 @@ import React from 'react';
 import styles from './PageTemplate.scss';
 import classNames from 'classnames/bind';
 
+import Header from '../Header';
+import Footer from '../Footer';
+
 const cx = classNames.bind(styles);
 
-const PageTemplate = () => (
+const PageTemplate = ({children}) => (
 	<div className={cx('page-tamplate')}>
-		
+		<Header />
+		{children}
+		<Footer />
 	</div>
 );
 
