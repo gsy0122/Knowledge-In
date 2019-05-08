7a6159eb3d4d4ddc685a0e70d8c5376eaff2e605
강서연 <gsy0122@dgsw.hs.kr>
2019-05-08 20:56:49 +0900

    [ADD] LoginPage

--- frontend/src/components/common/Header/Header.js
+++ frontend/src/components/common/Header/Header.js
@@ -8,11 +8,14 @@ class Header extends Component {
 	render() {
 		return(
 			<header className={cx('header')}>
-        <ul>
-          <li><a href="/">Q&A</a></li>
+        <ul class="center-menu">
+          <li><a href="/">홈</a></li>
+          <li><a href="/qna">Q&A</a></li>
           <li><a href="/question">질문하기</a></li>
           <li><a href="/answer">답변하기</a></li>
           <li><a href="/profile">프로필</a></li>
+          <li><a href="/register">회원가입</a></li>
+          <li><a href="/login">로그인</a></li>
         </ul>
       </header>
 		);
