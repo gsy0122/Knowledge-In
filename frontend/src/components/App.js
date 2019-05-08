26e66fcf48f500c5579df9d368b98b2f4618afbb
강서연 <gsy0122@dgsw.hs.kr>
2019-05-08 12:26:02 +0900

    [ADD] QuestionPage

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { HomePage } from '../pages';
+import { HomePage, QuestionPage } from '../pages';
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/question" component={QuestionPage} />
       </Switch>
     );
   }
---
+++ frontend/src/pages/QuestionPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageTemplate from './../components/common/PageTemplate';
+import Question from './../components/question/Question';
+
+const QuestionPage = () => (
+	<PageTemplate>
+		<Question />
+	</PageTemplate>
+);
+
+export default QuestionPage;
