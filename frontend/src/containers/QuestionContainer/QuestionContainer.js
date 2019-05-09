dab3b166ea3c7cd38e080f483990fcfd0aa58eb4
강서연 <gsy0122@dgsw.hs.kr>
2019-05-09 14:02:38 +0900

    [ADD] QuestionContainer

--- frontend/src/containers/QuestionContainer/QuestionContainer.js
+++ frontend/src/containers/QuestionContainer/QuestionContainer.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import styles from './LoginContainer.scss';
-import classNames from 'classnames/bind';
-
-const cx = classNames.bind(styles);
+import './LoginContainer.scss';
 
 class QuestionContainer extends Component {
 	render() {
 		return(
-			<div classNames={cx('question')}>
+			<div className='question'>
 				
 			</div>
 		);
