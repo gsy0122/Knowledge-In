0b3ffc1588c98e88db7319245df664069c38b9be
강서연 <gsy0122@dgsw.hs.kr>
2019-05-11 10:46:44 +0900

    [ADD] Comment

---
+++ frontend/src/components/base/Comment/Comment.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styles from './Comment.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+class Comment extends Component {
+	render() {
+		return(
+			<div className={cx('comment')}>
+				<textarea placeholder='명예 훼손 및 불법 정보 유통 시 모니터링 후 삭제될 수 있으며, 이에 대한 책임은 게시자에게 있습니다.'>
+				</textarea>
+				<button>등록</button>
+			</div>
+		);
+	}
+}
+
+export default Comment;
