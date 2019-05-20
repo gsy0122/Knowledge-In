19478312877efed713539b70e9ca440175518a4b
강서연 <gsy0122@dgsw.hs.kr>
2019-05-20 22:49:00 +0900

    [FIX] Register

--- frontend/src/components/auth/Register/Register.js
+++ frontend/src/components/auth/Register/Register.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Register = () => {
-	return(
-		<div>
-			이름 <input /><br />
-			아이디 <input /><br />
-			비밀번호 <input type='password' /><br />
-			이메일 <input /><br />
-			전화번호 <input /><br />
-			프로필 사진 <input type='file' /><br />
-		  상태 메시지 <input /><br />
-			<button>회원가입</button>
-		</div>
-	);
+class Register extends Component {
+  state = {
+    name: '',
+    id: '',
+    pw: '',
+    email: '',
+    mobile: '',
+    profile_image: '',
+    state_message: '',
+  };
+  render() {
+    const handleChange = e => {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    }
+    const handleImageChange = e => {
+      this.setState({
+        profile_image: e.target.files[0],
+      });
+    };
+    const handleSubmit = async () => {
+      await axios
+        .post('http://localhost:8000/member', this.state)
+        .then(response => {
+          alert('회원가입 성공');
+          console.log(JSON.stringify(response));
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    };
+    return(
+      <div>
+        이름 <input name='name' onChange={handleChange} /><br />
+        아이디 <input name='id' onChange={handleChange} /><br />
+        비밀번호 <input name='pw' type='password' onChange={handleChange} /><br />
+        이메일 <input name='email' onChange={handleChange} /><br />
+        전화번호 <input name='mobile' onChange={handleChange} /><br />
+        프로필 사진 <input name='profile_image' type='file' onChange={handleImageChange} /><br />
+        상태 메시지 <input name='state_message' onChange={handleChange} /><br />
+        <button onClick={handleSubmit}>회원가입</button>
+      </div>
+    ); // onClick 시 서버에서 member post
+  }
 };
 
 export default Register;
