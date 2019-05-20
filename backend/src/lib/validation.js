a057a3c69353b3db15e98c37f610e5387fb73811
강서연 <gsy0122@dgsw.hs.kr>
2019-05-20 16:08:42 +0900

    [ADD] validateMember

--- backend/src/lib/validation.js
+++ backend/src/lib/validation.js
@@ -1,6 +1,40 @@
 const BaseJoi = require('joi');
 const Joi = BaseJoi.extend(require('joi-date-extensions'));
 
+exports.validateMemberRegister = async (body) => {
+  const schema = BaseJoi.object().keys({
+    id: BaseJoi.string().regex(/^[a-z0-9]{4,20}$/).min(5).max(20),
+    pw: BaseJoi.string().regex(/^[!-}]{10,120}$/),
+    name: BaseJoi.string().min(1).max(20).required(),
+    mobile: BaseJoi.string().regex(/^[0-9]{7,16}$/).required(),
+    email: BaseJoi.string().min(8).email(),
+    profile_image: BaseJoi.string().allow(''),
+    status_message: Joi.string().allow(''),
+		auth: Joi.number().integer().required().min(1),
+  });
+
+  try {
+    return await Joi.validate(body, schema);
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.validateMemberModify = async (body) => {
+  const schema = {
+    mobile: Joi.string().required(),
+    email: Joi.string().min(8).email(),
+    status_message: Joi.string().required().allow(''),
+    profile_image: Joi.string().required().allow(''),
+  };
+
+  try {
+    return await Joi.validate(body, schema);
+  } catch (error) {
+    throw error;
+  }
+};
+
 exports.ValidateQuestion = async (body) => {
 	const schema = Joi.object().keys({
 		title: Joi.string().required().trim(),
