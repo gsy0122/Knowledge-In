889a190ad6cf5979c466f96494a5d52145cdce92
강서연 <gsy0122@dgsw.hs.kr>
2019-05-20 22:48:45 +0900

    [FIX] Login

--- frontend/src/components/auth/Login/Login.js
+++ frontend/src/components/auth/Login/Login.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import './Login.scss';
 
 const Login = () => {
-	const [id, setId] = React.useState('');
-	const [pw, setPw] = React.useState('');
-	return(
-		<div className='login'> 
-			<input name="id" placeholder="ID" value={id} onChange={e => { setId(e.target.value); }}/>
-			<input name="pw" placeholder="PASSWORD" value={pw} type="password" onChange={e => { setPw(e.target.value); }} />
-			<button>LOGIN</button> 
-		</div>
-	); // onClick 시 서버에서 member post
+  const [id, setId] = React.useState('');
+  const [pw, setPw] = React.useState('');
+  const handleSubmit = async () => {
+    await axios
+      .post('http://localhost:8000/auth/login', {id, pw})
+      .then(response => {
+        alert('로그인 성공');
+        console.log(JSON.stringify(response));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  return(
+    <div className='login'> 
+      <input name="id" placeholder="ID" value={id} onChange={e => { setId(e.target.value); }}/>
+      <input name="pw" placeholder="PASSWORD" value={pw} type="password" onChange={e => { setPw(e.target.value); }} />
+      <button onClick={handleSubmit}>LOGIN</button> 
+    </div>
+  );
 }
 
 export default Login;
