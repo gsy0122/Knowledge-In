6e688b65bd286a582f120b45e531f0508dd559f1
강서연 <gsy0122@dgsw.hs.kr>
2019-05-30 14:59:04 +0900

    [FIX] login component

--- frontend/src/components/auth/Login/Login.js
+++ frontend/src/components/auth/Login/Login.js
@@ -1,29 +1,49 @@
 import React from 'react';
 import axios from 'axios';
 import './Login.scss';
+import {withRouter} from 'react-router-dom';
 
-const Login = () => {
+const Login = ({history}) => {
   const [id, setId] = React.useState('');
   const [pw, setPw] = React.useState('');
-  const handleSubmit = async () => {
+  const handleLogin = async () => {
     await axios
       .post('http://localhost:8000/auth/login', {id, pw})
       .then(response => {
         alert('로그인 성공');
-        console.log(JSON.stringify(response));
+        localStorage.setItem('token', response.data.data.token);
+        history.push({pathname: '/profile'});
       })
       .catch(error => {
+        alert('로그인 실패');
         console.log(error);
       });
   };
+  const handleLogout = async () => {
+    if (localStorage.getItem('token') === null) {
+      alert('로그아웃 실패');
+      return;
+    }
+    await axios
+    .post('http://localhost:8000/member/logout', {device: localStorage.getItem('token')})
+    .then(response => {
+      alert('로그아웃 성공');
+      localStorage.removeItem('token');
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
 
   return(
     <div className='login'> 
       <input name="id" placeholder="ID" value={id} onChange={e => { setId(e.target.value); }}/>
       <input name="pw" placeholder="PASSWORD" value={pw} type="password" onChange={e => { setPw(e.target.value); }} />
-      <button onClick={handleSubmit}>LOGIN</button> 
+      <button onClick={handleLogin}>LOGIN</button> 
+      <button onClick={handleLogout}>LOGOUT</button>
     </div>
   );
 }
 
-export default Login;
+export default withRouter(Login);
