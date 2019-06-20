cae812987c1d5df2a3ed144940a36f15c9e3cce5
강서연 <gsy0122@dgsw.hs.kr>
2019-06-20 20:30:19 +0900

    [ADD] WriteQuestionContainer

---
+++ frontend/src/containers/WriteQuestionContainer/WriteQuestionContainer.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import {withRouter} from 'react-router-dom';
+
+import './WriteQuestionContainer.scss';
+import WriteQuestion from './../../components/question/WriteQuestion';
+
+@inject('stores')
+@observer
+class WriteQuestionContainer extends Component {
+  componentDidMount() {
+    this.props.stores.CategoryStore.getCategories();
+    console.log(this.props.match.params.question_id);
+    if (this.props.match && this.props.match.params.question_id) {
+      this.props.stores.QuestionStore.getQuestion(this.props.match.params.question_id);
+    }
+  }
+  render() {
+		const categories = this.props.stores.CategoryStore.categories;
+    const question = this.props.stores.QuestionStore.question;
+    console.log(question);
+    
+    if (this.props.match && this.props.match.params.question_id) {
+      return <WriteQuestion question={question} categories={categories} />
+    }
+    return(
+      <WriteQuestion categories={categories}/>
+    );
+  };
+}
+
+export default withRouter(WriteQuestionContainer);
