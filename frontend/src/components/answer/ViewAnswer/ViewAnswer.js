3198a3e946c3365b7ad5751262d5b7b28c2935e3
강서연 <gsy0122@dgsw.hs.kr>
2019-06-23 06:45:50 +0900

    [FIX] answer component

--- frontend/src/components/answer/ViewAnswer/ViewAnswer.js
+++ frontend/src/components/answer/ViewAnswer/ViewAnswer.js
@@ -1,32 +1,60 @@
 import React from 'react';
 import axios from 'axios';
+import styles from './ViewAnswer.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 const ViewAnswer = (props) => {
-  const onAdopt = async (answer) => {
+  const answer = props.answer;
+  const a = props.a;
+  const b = props.b;
+  const remove = async () => {
+    if (answer.isAdopted === 1) {
+      alert('채택된 답변은 삭제할 수 없습니다.');
+      return;
+    }
     await axios
-      .put(`http://localhost:8000/answer/${answer._id}`, { "isAdopted": 1, }, {
+    .delete(`http://localhost:8000/answer/${answer._id}`, {
+      headers: {
+        'x-access-token': localStorage.getItem('token'),
+      },
+    })
+    .then(response => {
+      alert('답변이 삭제되었습니다.');
+      console.log(JSON.stringify(response));
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  const onAdopt = async () => {
+    await axios
+      .put(`http://localhost:8000/answer/adopt/${answer._id}`, {
+
+      }, { 
         headers: {
           'x-access-token': localStorage.getItem('token'),
         },
       })
       .then(response => {
-        alert('채택 성공');
+        alert('채택되었습니다.');
         console.log(response);
       })
       .catch(error => {
-        alert('채택 실패');
+        alert('채택에 실패하였습니다.');
         console.log(error);
       });
   }
   return(
-    <div>
-      답변
-      {props.answers.map(answer => (
-        <div>
-          <p key={answer._id}>{answer.content}</p>
-          <button onClick={onAdopt.bind(this, answer)}>채택</button>
-        </div>
-      ))}
+    <div className={cx('view-answer')} key={answer._id}>
+      {answer.isAdopted === 1 && <div>채택된 답변입니다.</div>}
+      <div>{answer.member.name} ({answer.member.id}) 님의 답변</div>
+      <div>{answer.content}</div>
+      <div className={cx('answer-setting')}>
+        {a && <button onClick={onAdopt}>채택</button>}
+        {b && <button onClick={remove}>삭제</button>}
+      </div>
     </div>
   );
 }
