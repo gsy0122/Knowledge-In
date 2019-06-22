6fad1ad4fa861d9fa4d46587c37120587b45b5e8
강서연 <gsy0122@dgsw.hs.kr>
2019-06-23 06:48:00 +0900

    [FIX] Header

--- frontend/src/components/base/Header/Header.js
+++ frontend/src/components/base/Header/Header.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
+import axios from 'axios';
 import styles from './Header.scss';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
 class Header extends Component {
-	render() {
-		return(
-			<header className={cx('header')}>
-        <ul>
-          <li><a href="/">홈</a></li>
-          <li><a href="/qna">Q&A</a></li>
-          <li><a href="/question">질문하기</a></li>
-          <li><a href="/answer">답변하기</a></li>
-          <li><a href="/profile">프로필</a></li>
-          <li><a href="/auth/register">회원가입</a></li>
-          <li><a href="/auth/login">로그인</a></li>
-        </ul>
+  render() {
+    return(
+      <header className={cx('header')}>
+        <div className={cx('title')}>
+          지식iN
+          {localStorage.getItem('token') ? 
+            <button onClick={this.logout}>로그아웃</button> : 
+            <button onClick={this.login}>로그인</button>
+          }
+        </div>
+        <div className={cx('top-bar')}>
+          <ul>
+            <li><a href="/">홈</a></li>
+            <li><a href="/question">질문하기</a></li>
+            <li><a href="/qna">답변하기</a></li>
+            <li><a href="/profile">프로필</a></li>
+          </ul>
+        </div>
       </header>
-		);
-	}
+    );
+  }
+  
+  login = () => {
+    this.props.history.push('/auth/login');
+  }
+
+  logout = async () => {
+    if (localStorage.getItem('token') === null) {
+      alert('로그인 된 사용자가 없습니다.');
+      return;
+    }
+    await axios
+    .post('http://localhost:8000/member/logout', {device: localStorage.getItem('token')})
+    .then(response => {
+      alert('로그아웃에 성공하였습니다. 이용해 주셔서 감사합니다.');
+      localStorage.removeItem('token');
+      console.log(response);
+      this.props.history.push('/');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
 }
 
-export default Header;
+export default withRouter(Header);
