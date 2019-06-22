99891d817491267a1abfc2403bbb671900065f3d
강서연 <gsy0122@dgsw.hs.kr>
2019-06-23 06:34:09 +0900

    [FIX] member component

--- frontend/src/components/member/MemberPane/MemberPane.js
+++ frontend/src/components/member/MemberPane/MemberPane.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import './MemberPane.scss';
-import PageTemplate from './../../common/PageTemplate';
+import styles from './MemberPane.scss';
+import classNames from 'classnames/bind';
+import {withRouter} from 'react-router-dom';
+
+const cx = classNames.bind(styles);
 
 const MemberPane = (props) => {
-	const member = props.member;
-	return(
-		<PageTemplate>
-      <p>{member.id}</p>
-      <p>{member.name}</p>
-      <p>{member.level}</p>
-      <p>질문 수 {member.question_count}</p>
-      <p>답변 수 {member.answer_count}</p>
-      <p>채택 수 {member.adopt_count}</p>
-    </PageTemplate>
-	);
+  const member = props.member;
+  const onSetting = () => {
+    props.history.push('/profile/' + props.member._id);
+  }
+  return(
+    <div className={cx('member-pane')}>
+      <div>
+        <div>{member.name} ({member.id}) 님의 프로필입니다.</div>
+        <div>{member.statusMessage}</div>
+        <div>현재 등급은 {member.level}이며, {member.adoptPoint}점의 내공이 누적되었습니다.</div>
+        <div>질문 수 {member.questionCount}</div>
+        <div>답변 수 {member.answerCount}</div>
+        <div>채택 수 {member.adoptCount}</div>
+      </div>
+      <button onClick={onSetting}>설정</button>
+    </div>
+  );
 };
 
-export default MemberPane;
+export default withRouter(MemberPane);
--- frontend/src/components/member/MemberSetting/MemberSetting.js
+++ frontend/src/components/member/MemberSetting/MemberSetting.js
@@ -1,98 +1,84 @@
 import React, {Component} from 'react';
-import PageTemplate from '../../common/PageTemplate';
-
-import axios from 'axios';
+import styles from './MemberSetting.scss';
+import classNames from 'classnames/bind';
 import {withRouter} from 'react-router-dom';
+import axios from 'axios';
+
+const cx = classNames.bind(styles);
 
 class MemberSetting extends Component {
-	state = {
+  state = {
     pw: '',
-    // profile_image: '',
     status_message: '',
-	}
-	constructor(props) {
-		super(props);
-		this.state = {
-			...this.state,
-			pw: props.member.pw,
-			status_message: props.member.statusMessage,
-		}
-	}
-	render() {
-		const member = this.props.member;
-		return(
-			<PageTemplate>
-				<div>이름 {member.name}</div>
-				<div>아이디 {member.id}</div>
-				<div>비밀번호 <input type='password' name='pw' onChange={this.handleChange} /></div>
-				<div>상태 메시지 <input name='status_message' value={this.state.status_message} onChange={this.handleChange} /></div>
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...this.state,
+      pw: props.member.pw,
+      status_message: props.member.statusMessage,
+    }
+  }
+  render() {
+    const member = this.props.member;
+    return(
+      <div className={cx('member-setting')}>
+        <div>이름 {member.name}</div>
+        <div>아이디 {member.id}</div>
+        <div>비밀번호 <input type='password' name='pw' onChange={this.handleChange} /></div>
+        <div>상태 메시지 <input name='status_message' value={this.state.status_message} onChange={this.handleChange} /></div>
         <div>이메일 {member.email}</div>
-				<div>전화번호 {member.mobile}</div>
-        <button onClick={this.update}>수정</button>
-        <button onClick={this.remove}>탈퇴</button>
-      	<button onClick={this.logout}>로그아웃</button>
-			</PageTemplate>
-		);
-	}
+        <div>전화번호 {member.mobile}</div>
+        <div>
+          <button onClick={this.update}>수정</button>
+          <p onClick={this.remove}>탈퇴하시겠습니까?</p>
+        </div>
+      </div>
+    );
+  }
 
-	handleChange = e => {
-		this.setState({
-			[e.target.name]: e.target.value
-		});
-	}
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-	update = async () => {
-		await axios
+  update = async () => {
+    await axios
       .put(`http://localhost:8000/member/${this.props.member._id}`, this.state, {
         headers: {
           'x-access-token': localStorage.getItem('token'),
         },
       })
       .then(response => {
-        alert('멤버 수정 성공');
-				console.log(JSON.stringify(response));
-				this.props.history.push({pathname: '/profile'});
+        alert('프로필 수정에 성공하였습니다.');
+        console.log(JSON.stringify(response));
+        this.props.history.push({pathname: '/profile'});
       })
       .catch(error => {
-        alert('멤버 수정 실패');
+        alert('프로필 수정에 실패하였습니다.');
         console.log(error);
       });
-	};
+  };
 
-	remove = async () => {
-		await axios
+  remove = async () => {
+    await axios
     .delete(`http://localhost:8000/member/${this.props.member._id}`, {
       headers: {
         'x-access-token': localStorage.getItem('token'),
       },
     })
     .then(response => {
-      alert('멤버 삭제 성공');
-			console.log(JSON.stringify(response));
-			localStorage.removeItem('token');
-			this.props.history.push({pathname: '/'});
-    })
-    .catch(error => {
-      console.log(error);
-    });
-	};
-
-	logout = async () => {
-    if (localStorage.getItem('token') === null) {
-      alert('로그아웃 실패');
-      return;
-    }
-    await axios
-    .post('http://localhost:8000/member/logout', {device: localStorage.getItem('token')})
-    .then(response => {
-      alert('로그아웃 성공');
+      alert('회원 탈퇴에 성공하였습니다. 이용해 주셔서 감사합니다.');
+      console.log(JSON.stringify(response));
       localStorage.removeItem('token');
-      console.log(response);
+      this.props.history.push({pathname: '/'});
     })
     .catch(error => {
+      alert('회원 탈퇴에 실패하였습니다. 못 나간다!');
       console.log(error);
     });
-  }
+  };
 }
 
 export default withRouter(MemberSetting);
