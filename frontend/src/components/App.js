d0333aa007e21f86f463b0ca0e2f37684e96915d
강서연 <gsy0122@dgsw.hs.kr>
2019-06-23 06:48:45 +0900

    [FIX] App

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { HomePage, QuestionPage, AnswerPage, AuthPage } from '../pages';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Provider } from 'mobx-react';
+
+import { HomePage, QuestionPage, AuthPage } from '../pages';
+import ProfileContainer from './../containers/ProfileContainer';
+import Stores from './../stores';
+import QuestionListContainer from './../containers/QuestionListContainer';
+import WriteQuestionContainer from './../containers/WriteQuestionContainer';
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/question" component={QuestionPage} />
-        <Route exact path="/answer" component={AnswerPage} />
-        <Route exact path="/auth/:kind" component={AuthPage} />
-      </Switch>
+      <Provider stores={Stores}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/question/:question_id?" render={
+            () => (localStorage.getItem('token') ? <WriteQuestionContainer /> : <Redirect to="/auth/login" />)
+          } />
+          <Route exact path="/qna/:category_id?" render={
+            () => (localStorage.getItem('token') ? <QuestionListContainer /> : <Redirect to="/auth/login" />)
+          } />
+          <Route exact path="/auth/:kind" component={AuthPage} />
+          <Route exact path="/question/view/:question_id" component={QuestionPage} />
+          <Route exact path="/profile/:member_id?" render={
+            () => (localStorage.getItem('token') ? <ProfileContainer /> : <Redirect to="/auth/login" />)
+          } />
+        </Switch>
+      </Provider>
     );
   }
 }
